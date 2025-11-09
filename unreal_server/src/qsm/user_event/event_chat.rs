d813c33104f36b43b3eba6bf143e1d5426802cb8
kimmo <hemakim0215@naver.com>
2025-11-09 11:33:47 +0900

    .

--- unreal_server/src/qsm/user_event/event_chat.rs
+++ unreal_server/src/qsm/user_event/event_chat.rs
@@ -1,9 +1,9 @@
 
 use crate::qsm::user_message::message_chat::{self, ChatMessage};
 use super::Network::server_common::*;
-
-
-
+use crate::Event::event_handler::EventHeader;
+use crate::qsm::qsm::GLOBAL_MESSAGE_TX_QUEUE;
+use crate::Network::connection::MessageToSend;
 pub fn CallBack_Chat(buffer: &[u8])
 {
     match ChatMessage::deserialize(buffer) {
@@ -11,13 +11,23 @@ pub fn CallBack_Chat(buffer: &[u8])
             let sender = chat_message.id;
             let chat_content = chat_message.content;
 
+            // let client_token = Token(client_token_value.try_into().unwrap());
+
             println!("sender id : {}", sender);
             println!("chat content : {}", chat_content);
 
-            if let Err(_) = GLOBAL_SERVER_ACTION_QUEUE.push(ServerActionType::ChatMessage(sender, chat_content)) {
-                eprintln!("Failed to queue Server Action EnterPlayer");
+            let mut _chat_message = ChatMessage::new(
+                EventHeader::CHAT_MESSAGE as u32,
+                sender,
+                chat_content.clone(),
+            );
+
+            let send_msg = _chat_message.serialize();
+
+            if let Err(_) = GLOBAL_MESSAGE_TX_QUEUE.push(MessageToSend::Broadcast(send_msg)) {
+                // eprintln!("Failed to queue AllowConnectGame message for client {:?}", client_token);
             } else {
-                println!("Queued Server Action EnterPlayer");
+                // println!("Queued AllowConnectGame message for client {:?}", client_token);
             }
         }
         Err(e) => {
