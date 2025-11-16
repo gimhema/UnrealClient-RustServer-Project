162f44bb2234704eb98b3315df89a1bb40acf8c6
kimmo <hemakim0215@naver.com>
2025-11-16 09:27:54 +0900

    .

--- unreal_server/src/qsm/user_event/event_player_info.rs
+++ unreal_server/src/qsm/user_event/event_player_info.rs
@@ -2,10 +2,20 @@
 use crate::Event::event_handler::EventHeader;
 use crate::qsm::qsm::GLOBAL_MESSAGE_TX_QUEUE;
 use crate::Network::connection::MessageToSend;
+use crate::qsm::user_message::message_set_user_profile_info::SetPlayerInfo;
 
 pub fn Callback_SetPlayerInfo(buffer: &[u8]) {
  
- 
-     println!("Received SetPlayerInfo event with buffer size: {}", buffer.len());
+    match SetPlayerInfo::deserialize(buffer) {
+        Ok(set_player_info_msg) => {
+            let player_id = set_player_info_msg.pId;
+//            let profile_info = set_player_info_msg.profile_info.clone();
 
- }
+        }
+        Err(e) => {
+            eprintln!("Failed to deserialize SetPlayerInfo: {}", e);
+        }
+    }   
+
+}
+
